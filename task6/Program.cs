1e84ee75b580c7af624fa8df0f8b7f828137a8be
akosnyrev <arezz@rambler.ru>
2022-08-08 11:25:58 +0500

    task6 done

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число:");
+double a = Convert.ToInt32( Console.ReadLine());
+
+if (a%2==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
