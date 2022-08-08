5c5b92869c54d2b5751c5ebf5e1223f0682e8de1
akosnyrev <arezz@rambler.ru>
2022-08-08 09:52:42 +0500

    task2 done

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число:");
+int a = Convert.ToInt32( Console.ReadLine());
+Console.Write("Введите второе число, не равное первому:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+Console.Write("Число ");
+Console.Write(a);
+Console.Write(" больше числа ");
+Console.WriteLine(b);
+}
+else
+{
+Console.Write("Число ");
+Console.Write(a);
+Console.Write(" Меньше числа ");
+Console.WriteLine(b);
+
+}
+
