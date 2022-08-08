b9cc441d5a85d56e25a164efb454c6183bdf51f4
akosnyrev <arezz@rambler.ru>
2022-08-08 12:35:41 +0500

    task15 done

---
+++ task15/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите номер дня недели:");
+int a=Convert.ToInt32(Console.ReadLine());
+if ((a==6)^(a==7))
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Рабочий");
+}
