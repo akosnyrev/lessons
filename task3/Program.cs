1949385d1085d5cf5585361dc2483678e56ff719
akosnyrev <arezz@rambler.ru>
2022-08-08 10:26:21 +0500

    task3 done

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] week = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+Console.WriteLine("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>=1 & a<=7)
+{
+Console.WriteLine(week[a-1]);
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}
