82ed2419ca2f5d65baabf38d512e817e1d394b40
akosnyrev <arezz@rambler.ru>
2022-08-08 11:21:51 +0500

    task5 done

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int i=-1*a;
+Console.WriteLine(i);
+for (;i<=a;i++)
+{
+    Console.Write(i);
+    Console.Write(" ");
+    }
