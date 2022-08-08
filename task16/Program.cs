df0a0b90273f7bfdf009bee04a32032eac1c300b
akosnyrev <arezz@rambler.ru>
2022-08-08 12:43:29 +0500

    task16 done

---
+++ task16/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+if ((a*a==b)^(b*b==a))
+{
+Console.WriteLine("ДА");
+}
+else
+{
+Console.WriteLine("НЕТ");
+}
