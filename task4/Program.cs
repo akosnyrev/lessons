a3a18a314a4233811cb7e1e71b2c0ded90eb883f
akosnyrev <arezz@rambler.ru>
2022-08-08 10:51:38 +0500

    task4 done

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array=new int[3];
+int i_max=0;
+Console.WriteLine("Введите три числа:");
+for (int i = 0; i < array.Length; i++)
+{
+   array[i]= Convert.ToInt32( Console.ReadLine());
+    if (array[i]>array[i_max])
+    {
+        i_max=i;
+    }
+}
+Console.Write("Максимальное введенное число: ");
+Console.WriteLine(array[i_max]);
+
