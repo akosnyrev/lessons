bdf0af0283af4eb65522d38a1d2cbe982ebbf13a
akosnyrev <arezz@rambler.ru>
2022-08-08 12:12:26 +0500

    task12 done

---
+++ task12/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите первое число:");
+int a=Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b=Convert.ToInt32( Console.ReadLine());
+if ((a%b)==0)
+{
+    Console.WriteLine("Кратно");
+}
+else 
+{
+    Console.WriteLine("Не кратно, остаток ");
+    Console.WriteLine(a%b);
+}
