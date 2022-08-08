83ac91b8f318f2a1701df8d94e7a571d9018c85a
akosnyrev <arezz@rambler.ru>
2022-08-08 09:21:52 +0500

    task1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число:");
+double a = Convert.ToInt32( Console.ReadLine());
+Console.Write("Введите второе число:");
+double b = Convert.ToInt32(Console.ReadLine());
+if (a/b==b)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
