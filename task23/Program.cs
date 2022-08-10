c21a23eeabddfc5d6b13ce2044b7dbec73491b00
akosnyrev <arezz@rambler.ru>
2022-08-10 12:27:43 +0500

    task23 done

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("введите число:");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ниже представлен ряд кубов всех чисел до {0:G}",n);
+for (double i=1;i<(Convert.ToDouble(n)+1);i++)
+{
+    Console.Write(" {0:G}",Math.Pow(i,3));
+}
