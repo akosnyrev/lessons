f5cc3ca65aa461147704a250022d989eda111690
akosnyrev <arezz@rambler.ru>
2022-08-10 14:18:24 +0500

    task27 done

---
+++ task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+sum = sum + n % 10;
+n = n /10 ;
+}
+ Console.WriteLine("Сумма цифр в вашем числе - {0:G}",sum);
