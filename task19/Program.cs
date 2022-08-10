c3e5c45ba02e0777bd3be32fd1ba2c0dff39d8d3
akosnyrev <arezz@rambler.ru>
2022-08-10 11:10:27 +0500

    task19 done

---
+++ task19/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Введите пятизначное число");
+bool flag=false;
+int num = Convert.ToInt32(Console.ReadLine());
+while (flag==false)
+{
+    
+    if (5==Convert.ToString(num).Length )
+        {
+            flag=true;
+        }
+        else
+        {
+            Console.WriteLine("Введено не пятизначное число, повторите ввод:");
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+}
+string str=Convert.ToString(num);
+if ((str[0]==str[4])&(str[2]==str[3]))
+    {
+        Console.WriteLine("Число - полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число - не полиндром");
+    }
