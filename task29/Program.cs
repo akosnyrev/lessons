159d4e9224263d50996d9dc6fa5fa47a12757ad6
akosnyrev <arezz@rambler.ru>
2022-08-10 14:30:08 +0500

    task29 done

---
+++ task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Ниже ваш массив из 8 случайны чисел");
+Random rnd=new Random();
+int[] array=new int[8];
+for (int i=0;i<8;i++)
+{
+    array[i]=rnd.Next();
+    Console.WriteLine(array[i]); 
+}
