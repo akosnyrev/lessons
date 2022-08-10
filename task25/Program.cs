c37542199af9497a2e1d4829354374c39d141dc3
akosnyrev <arezz@rambler.ru>
2022-08-10 12:58:06 +0500

    task25 done

---
+++ task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Давайте возведем число А в степень В");
+Console.WriteLine("Для этого введите число А:");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Теперь число В:");
+int b=Convert.ToInt32(Console.ReadLine());
+int result=a;
+for (int i=2;i<(b+1);i++)
+{
+    result=result*a;
+}
+Console.WriteLine("Результат возведения в степень равен {0:G}",result);
