82a19f82ba39eab03faf5b62229ba04a021111d8
akosnyrev <arezz@rambler.ru>
2022-08-23 21:44:05 +0500

    task 54 56 58 59 64 66 68 done

---
+++ task68/task68.cs
@@ -0,0 +1,50 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 3, n = 2 -> A(m,n) = 29
+
+
+int A(int m,int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return A(m - 1, 1);
+  else return A(m - 1, A(m, n - 1));
+}
+Console.WriteLine("Введите m для функции Аккермана:");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n для функции Аккермана:");
+int n=Convert.ToInt32(Console.ReadLine());
+if ((n<0)^(m<0))
+{Console.WriteLine("Числа не могут быть отрицательными");
+}
+else
+{
+Console.WriteLine($"Функция аккремана при m={m} n={n} равняется {A(m, n)}");
+}
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+/*
+Console.Clear();
+Console.WriteLine($"Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+*/
